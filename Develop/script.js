860f502ce607e2b18c752ce12c16a67aaef7f8a1
Illgner Alain <illgneralain@gmail.com>
2021-04-02 02:12:47 -0400

    created Password object

--- Develop/script.js
+++ Develop/script.js
@@ -13,9 +13,23 @@ function writePassword() {
 // Password Variable 
 
 // Prompt Variables of:
+
+var Password = {
+  "length":  129,
+  "upperCase": [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"],
+  "lowerCase": ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"],
+  "numeric": ["1", "2", "3", "4", "5", "6", "7", "8", "9"] ,
+  "specialChar": ["!", "@", "#", "$", "%", "^", "&", "*", "*", "(", ")"]  
+};
+
+console.log(Password) 
+// to check if object successfully created
+
 // length >= 8 characters && length <= 129 characters 
-// character types: uppercase, lowercase, special characters 
+// character types: uppercase, lowercase, numeric, special characters 
 // input variable that loops through answers to prompts so at least 1 of each char type is selected
+
+
 // result variable should be called to display selected input
 // password can be displayed as an alert as well 
 
